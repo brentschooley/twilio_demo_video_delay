ed6cf70e97abcd1f69e442e6220ced49f6580bcb
Brent Schooley <schooleybrent@gmail.com>
2020-12-01 00:41:36 -0500

    Updated code for 2020.

--- server.js
+++ server.js
@@ -1,56 +1,60 @@
-const express = require('express');
-const schedule = require('node-schedule');
-const moment = require('moment');
-const bodyParser = require('body-parser');
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
-const VoiceResponse = require('twilio').twiml.VoiceResponse;
+const express = require("express");
+const schedule = require("node-schedule");
+const moment = require("moment");
+const bodyParser = require("body-parser");
+const MessagingResponse = require("twilio").twiml.MessagingResponse;
+const VoiceResponse = require("twilio").twiml.VoiceResponse;
 
 // Twilio Credentials
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 
 // require the Twilio module and create a REST client
-const client = require('twilio')(accountSid, authToken);
+const client = require("twilio")(accountSid, authToken);
 
 const app = express();
-app.set('port', (process.env.PORT || 5000));
-app.use(bodyParser());
-app.use(express.static('public'));
+app.set("port", process.env.PORT || 5000);
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static("public"));
 
-app.post('/sms', (req, res) => {
-  console.log('Hi!');
-  var date = moment().add(process.env.VIDEO_DELAY_SECONDS, 's').toDate();
+app.post("/sms", (req, res) => {
+  console.log("Hi!");
+  var date = moment().add(process.env.VIDEO_DELAY_SECONDS, "s").toDate();
   const from = req.body.From;
-  console.log(date);
-  console.log(from);
-  var j = schedule.scheduleJob(date, function(y){
-    client.calls.create({
-      url: process.env.VOICE_TWIML_URL,
-      to: from,
-      from: process.env.TWILIO_PHONE_NUMBER,
-    })
-  }.bind(null, from));
+
+  schedule.scheduleJob(
+    date,
+    function () {
+      client.calls.create({
+        url: `${req.protocol}://${req.hostname}/voice_response`,
+        to: from,
+        from: process.env.TWILIO_PHONE_NUMBER,
+      });
+    }.bind(null, from)
+  );
 
   const twiml = new MessagingResponse();
 
   twiml.message(
-`Thanks for messaging my demo!
+`
+Thanks for messaging my demo!
 Find out more about Twilio in the docs at https://twilio.com/docs
-Check us out on YouTube at https://youtube.com/TeamTwilio`
+Check us out on YouTube at https://youtube.com/TwilioDevs
+`
   );
 
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+  res.writeHead(200, { "Content-Type": "text/xml" });
   res.end(twiml.toString());
 });
 
-app.post('/voice_response', (req, res) => {
+app.post("/voice_response", (req, res) => {
   const twiml = new VoiceResponse();
-  twiml.play({}, 'https://intense-reef-61003.herokuapp.com/that_one_song.mp3');
+  twiml.play({}, `${req.protocol}://${req.hostname}/that_one_song.mp3`);
 
-  res.type('text/xml');
+  res.type("text/xml");
   res.send(twiml.toString());
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Node app is running on port", app.get("port"));
 });
